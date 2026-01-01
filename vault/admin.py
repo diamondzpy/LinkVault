03563d2204001ed0d265aa7317b930de31fe366d
Oscar Zhou <1oscarzhou@gmail.com>
2026-01-01 13:28:25 -0500

    Added bookmarks and tag models

--- vault/admin.py
+++ vault/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Bookmark, Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("title", "url", "updated_at")
+    search_fields = ("title", "url", "notes")
+    list_filter = ("tags",)
--- vault/models.py
+++ vault/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Bookmark(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=120)
+    notes = models.TextField(blank=True)
+
+    tags = models.ManyToManyField(Tag, blank=True, related_name="bookmarks")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
